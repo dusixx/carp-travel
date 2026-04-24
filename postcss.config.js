37d7b1e6215ccb0556bb65b1b1fc9101d534e10b
dusixx <113414935+dusixx@users.noreply.github.com>
2026-04-25 01:35:02 +0300

    chore: update packages and configs

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
-module.exports = {
-  plugins: {
-    'postcss-import': {},
-    'tailwindcss/nesting': 'postcss-nesting',
-    tailwindcss: {},
-    autoprefixer: {},
-    // 'postcss-preset-env': {
-    //   features: { 'nesting-rules': false },
-    // },
-  },
+export const plugins = {
+  'postcss-import': {},
+  'tailwindcss/nesting': 'postcss-nesting',
+  tailwindcss: {},
+  autoprefixer: {},
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,9 +16,9 @@ export const PAGE_HEADER_GRADIENT_TER =
 
 const config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     transitionDuration: {
